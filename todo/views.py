fcf2210260a823bc2a57b9cb7c2e797e16b237a7
Elliot Redhead <elliot.redhead@gmail.com>
2020-02-20 10:48:47 +0000

    Corrected function spacing.

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
 def say_hello(request):
+    """
+    Creates a simple "Hello World" view to test if basic system is functioning.
+    """
     return HttpResponse("Hello World")
+
+
+def get_todo_list(request):
+    """
+    Returns the basic to-do list page.
+    """
+    return render(request, "todo_list.html")
