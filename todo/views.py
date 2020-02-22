6b1822c41650809ea5bf5a325a6cb7b568228b5a
ElliotRedhead <elliot.redhead@gmail.com>
2020-02-22 23:35:37 +0000

    Redirects to list on new item submit.

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .models import Item
 
 # Create your views here.
@@ -11,8 +11,11 @@ def get_todo_list(request):
     results = Item.objects.all()
     return render(request, "todo_list.html", {"items": results})
 
+
 def create_an_item(request):
     """
     Generates a form for the user to create a new item.
     """
+    if request.method == "POST":
+        return redirect(get_todo_list)
     return render(request, "item_form.html")
