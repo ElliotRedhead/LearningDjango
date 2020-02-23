a02af8dc53e95c64d5b835f993566589626c8376
ElliotRedhead <elliot.redhead@gmail.com>
2020-02-23 20:37:06 +0000

    Test to check if form is valid with name only.

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
-
+from .forms import ItemForm
 # Create your tests here.
 
 
-class TestDjango(TestCase):
-    def test_is_this_thing_on(self):
-        self.assertEqual(1, 1)
+class TestToDoItemForm(TestCase):
+    def test_can_create_an_item_with_just_a_name(self):
+        form = ItemForm({"name": "Create Tests"})
+        self.assertTrue(form.is_valid())
