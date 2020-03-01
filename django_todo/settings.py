594c1c9e970ca0a63577c8fc2b12d71388686350
Elliot Redhead <elliot.redhead@gmail.com>
2020-03-01 15:34:05 +0000

    Removed unused import.

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 import dj_database_url
-import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,7 +28,6 @@ DEBUG = True
 
 if "HOSTNAME" in os.environ:
     ALLOWED_HOSTS = os.environ.get("HOSTNAME")
-
 
 
 # Application definition
