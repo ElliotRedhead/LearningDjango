2252597af3bd724a15949044e4a6a9badb42e9df
Elliot Redhead <elliot.redhead@gmail.com>
2020-03-01 17:08:36 +0000

    Moved new secret key to env variable.

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5@rx%!8cd&=r!#m@q7c&kwimp685a&u)&oaguvh(hav5h4aj0+'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = development
